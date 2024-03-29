65491c27e640471fd6e82342d1818d1aa1183f83
nicejji <nicejji@proton.me>
2024-03-29 15:38:31 +0300

    add catalog page

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,13 +1,17 @@
 import prisma from "@/prisma/client";
 import { Phone } from "@prisma/client";
+import Link from "next/link";
 
 const PhoneCard = ({ phone }: { phone: Phone }) => {
 	return (
 		<div className="flex flex-col gap-3 p-3 bg-blue-100 rounded-lg w-fit">
-			<h3 className="flex gap-3 items-center text-xl">
+			<Link
+				href={`/items/${phone.id}`}
+				className="flex gap-3 items-center text-xl"
+			>
 				<span className="font-bold">{phone.manufacturer}</span>
 				<span>{phone.model}</span>
-			</h3>
+			</Link>
 			<img className="h-64 rounded-lg aspect-square" src={phone.imageLink} />
 			<span className="flex gap-2">
 				Стоимость
