1e1461fccdc03529d3d4452edf4c201636218853
nicejji <nicejji@proton.me>
2024-03-29 15:38:04 +0300

    add auth pages

--- app/auth/create/page.tsx
+++ app/auth/create/page.tsx
@@ -1,12 +1,10 @@
+import SignUpForm from "./form";
+
 export default function AuthPage() {
 	return (
 		<div className="flex flex-col gap-5 justify-center items-center w-full h-full">
 			<h1 className="text-2xl font-bold text-blue-500">Создать аккаунт</h1>
-			<form className="flex flex-col gap-3 items-center">
-				<input type="email" placeholder="Электронная почта" className="input" />
-				<input type="password" placeholder="Пароль" className="input" />
-				<button className="btn btn-filled">Создать</button>
-			</form>
+			<SignUpForm />
 		</div>
 	);
 }
