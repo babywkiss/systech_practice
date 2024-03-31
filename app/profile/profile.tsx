6d190f947bb66a7946ded64e9056101c1d8f5ee9
nicejji <nicejji@proton.me>
2024-03-31 03:25:22 +0300

    counted items in basket

--- app/profile/profile.tsx
+++ app/profile/profile.tsx
@@ -4,8 +4,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { logoutUser, removeItemFromBasket } from "../store/userSlice";
 import { RootState } from "../store/store";
 import { useRouter } from "next/navigation";
+import { Phone } from "@prisma/client";
+import Link from "next/link";
+
+const getUniquePhones = (phones: Phone[]) => {
+	const unique: Phone[] = [];
+	const viewedIds = new Set<number>();
+	for (const phone of phones) {
+		if (!viewedIds.has(phone.id)) {
+			unique.push(phone);
+			viewedIds.add(phone.id);
+		}
+	}
+	return unique;
+};
+
+const getCountedBasket = (phones: Phone[]) => {
+	const unique = getUniquePhones(phones).map((phone) => ({ phone, count: 0 }));
+	for (const phone of phones) {
+		const target = unique.find((p) => p.phone.id === phone.id);
+		if (target) target.count += 1;
+	}
+	return unique;
+};
 
 export default function Profile() {
+	// TODO: add pay page
 	const user = useSelector((state: RootState) => state.user);
 	const basket = useSelector((state: RootState) => state.basket);
 	const router = useRouter();
@@ -24,24 +48,39 @@ export default function Profile() {
 				Выйти из аккаунта
 			</button>
 			<span className="text-xl font-bold">Ваша корзина</span>
+			<button className="bg-green-500 btn btn-filled">Оформить заказ</button>
+			<span className="text-xl font-bold">
+				Общая стоимость:{" "}
+				{basket.reduce((total, phone) => total + phone.priceBYN, 0) / 100} BYN
+			</span>
 			<ul className="flex gap-2">
-				{basket.map((item) => (
+				{getCountedBasket(basket).map(({ phone, count }) => (
 					<div
-						key={item.id}
+						key={phone.id}
 						className="flex flex-col items-center p-3 bg-blue-200 rounded-lg"
 					>
-						<img className="h-32" src={item.imageLink} />
+						<img className="h-32" src={phone.imageLink} />
 						<span>
-							{item.manufacturer} - {item.model}
+							{phone.manufacturer} - {phone.model}
 						</span>
+						<Link
+							href={`/items/${phone.id}`}
+							className="bg-gray-400 btn btn-filled"
+						>
+							Подробнее
+						</Link>
 						<button
 							onClick={() => {
-								dispatch(removeItemFromBasket(item));
+								dispatch(removeItemFromBasket(phone));
 							}}
 							className="bg-orange-500 btn btn-filled"
 						>
 							Удалить из корзины
 						</button>
+						<span className="font-bold">Штук: {count}</span>
+						<span className="font-bold">
+							Стоимость: {phone.priceBYN / 100} * {count}
+						</span>
 					</div>
 				))}
 			</ul>
