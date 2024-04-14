f23cce2a961ca237528457f90c082d9949e7850f
nicejji <nicejji@proton.me>
2024-04-14 16:08:06 +0300

    seed phones sequantially

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -63,14 +63,14 @@ async function main() {
 		"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.itl.cat%2Fpngfile%2Fbig%2F206-2063899_cute-kitten-images-hd.jpg&f=1&nofb=1&ipt=fc3197f023316bd66d0f9a75332559d6b103faffea55a97c95e33995886f450d&ipo=images",
 		"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F9%2F9b%2FPhoto_of_a_kitten.jpg&f=1&nofb=1&ipt=34710762570cc5c3e67e5b4935cfcd3797d5e9c4634708d4e47419b4344ce30a&ipo=images",
 	];
-	await Promise.all([
-		...Array.from({ length: 150 }, () =>
-			prisma.phone.create({ data: createRandomPhone() }),
-		),
-		...promoLinks.map((link) =>
+	for (let i = 0; i < 150; i++) {
+		await prisma.phone.create({ data: createRandomPhone() });
+	}
+	await Promise.all(
+		promoLinks.map((link) =>
 			prisma.promo.create({ data: { imageLink: link } }),
 		),
-	]);
+	);
 }
 
 main()
