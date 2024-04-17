737b32868dcd61b46314f709fe66a3765c8dfdd0
nicejji <nicejji@proton.me>
2024-04-17 14:49:33 +0300

    one time password resets

--- app/resetPassword/confirm/page.tsx
+++ app/resetPassword/confirm/page.tsx
@@ -1,6 +1,5 @@
 import { decodeToken } from "@/app/api/auth";
 import prisma from "@/prisma/client";
-import { IconKey } from "@tabler/icons-react";
 import ConfirmResetForm from "./confirm-reset-form";
 
 const InvalidMessage = () => <h1>Ссылка не действительна</h1>;
@@ -13,10 +12,16 @@ export default async function ConfirmPage({
 	const data = await decodeToken(searchParams?.token ?? "");
 	const id = data?.payload?.id;
 	const passwordReset = data?.payload?.passwordReset === true;
+	const resetCount = data?.payload?.resetCount;
 
-	if (typeof id !== "number" || !passwordReset) return <InvalidMessage />;
+	if (
+		typeof id !== "number" ||
+		!passwordReset ||
+		typeof resetCount !== "number"
+	)
+		return <InvalidMessage />;
 	const user = await prisma.user.findUnique({ where: { id } });
-	if (!user) return <InvalidMessage />;
+	if (!user || user.passwordResets !== resetCount) return <InvalidMessage />;
 
 	return <ConfirmResetForm token={searchParams?.token ?? ""} user={user} />;
 }
