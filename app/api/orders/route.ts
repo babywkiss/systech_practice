40ea76a6a5da35b1f869b84861ea15e98f983b01
nicejji <nicejji@proton.me>
2024-04-18 01:12:00 +0300

    refactor backend code for payments

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,106 +1,76 @@
-import { Phone } from "@prisma/client";
 import stripe from "./stripe";
 import prisma from "@/prisma/client";
-import { PaymentIntent } from "@stripe/stripe-js";
 import { extractUser } from "../auth";
+import { errorResponse, tryOrNull } from "../utils";
+import { Basket, validateBasket } from "../basket";
+import { User } from "@prisma/client";
 
-type Basket = { count: number; phone: Phone }[];
+type RequestData = {
+	basket?: any;
+	payment_method_id?: string;
+	payment_intent_id?: string;
+};
+
+const createIntent = async (reqData: RequestData, totalPrice: number) => {
+	if (reqData?.payment_method_id) {
+		return await stripe.paymentIntents.create({
+			payment_method_types: ["card"],
+			payment_method: reqData.payment_method_id,
+			amount: totalPrice as number,
+			currency: "byn",
+			confirm: true,
+			confirmation_method: "manual",
+		});
+	}
+
+	if (reqData?.payment_intent_id) {
+		return await stripe.paymentIntents.confirm(reqData.payment_intent_id);
+	}
+
+	return null;
+};
+
+const placeOrder = async (basket: Basket, totalPrice: number, user: User) => {
+	const flatBasket = basket.reduce((acc, { count, phone }) => {
+		for (let i = 0; i < count; i++) {
+			acc.push(phone.id);
+		}
+		return acc;
+	}, [] as number[]);
+
+	await prisma.order.create({
+		data: {
+			totalPriceBYN: totalPrice,
+			customerId: user.id,
+			items: {
+				connect: flatBasket.map((id) => ({ id })),
+			},
+		},
+	});
+};
 
 export async function POST(request: Request) {
 	const user = await extractUser();
 	if (!user) return Response.json({ error: "Not authorized" }, { status: 500 });
 
-	let totalPrice = 0;
-	let basket = null as null | Basket;
-	let intent: PaymentIntent | null = null;
+	const data = (await tryOrNull(request.json())) ?? {};
 
-	try {
-		const data = (await request.json()) as {
-			basket: Basket;
-			payment_method_id?: string;
-			payment_intent_id?: string;
-		};
+	const { error, totalPrice } = await validateBasket(data?.basket);
+	if (error) return errorResponse(error, 400);
 
-		// TODO: Validate basket schmea before iteration
-		basket = data.basket;
-		for (const { count, phone } of basket) {
-			const found = await prisma.phone.findUnique({ where: { id: phone.id } });
-			if (!found)
-				return Response.json({ error: `Phone #${phone.id} not found.` });
-			if (found.available_quantity < count)
+	const intent = await createIntent(data, totalPrice as number);
+
+	switch (intent?.status) {
+		case "succeeded":
+			await placeOrder(data.basket, totalPrice as number, user);
+			return Response.json({ success: true });
+		case "requires_action":
+			if (intent.next_action?.type === "use_stripe_sdk")
 				return Response.json({
-					error: `Quantity of ${count} is not available for phone ${phone.id}`,
+					requires_action: true,
+					payment_intent_client_secret: intent.client_secret,
 				});
-			totalPrice += found.priceBYN * count;
-		}
-
-		if (data.payment_method_id) {
-			intent = (await stripe.paymentIntents.create({
-				payment_method_types: ["card"],
-				payment_method: data.payment_method_id,
-				amount: totalPrice,
-				currency: "byn",
-				confirm: true,
-				confirmation_method: "manual",
-			})) as PaymentIntent;
-		} else if (data.payment_intent_id) {
-			intent = (await stripe.paymentIntents.confirm(
-				data.payment_intent_id,
-			)) as PaymentIntent;
-		}
-		if (intent) {
-			const response = generateResponse(intent);
-			if (response.success) {
-				// create order
-				const flatBasket = basket.reduce((acc, { count, phone }) => {
-					for (let i = 0; i < count; i++) {
-						acc.push(phone.id);
-					}
-					return acc;
-				}, [] as number[]);
-				await prisma.order.create({
-					data: {
-						totalPriceBYN: totalPrice,
-						customerId: user.id,
-						items: {
-							connect: flatBasket.map((id) => ({ id })),
-						},
-					},
-				});
-			}
-			return Response.json(response);
-		}
-		return Response.json({ error: "no payment id" }, { status: 400 });
-	} catch (e) {
-		console.log(e);
-		return Response.json(
-			{ error: `failed to create payment` },
-			{ status: 400 },
-		);
+		default:
+			return errorResponse("Unknown error", 500);
 	}
 }
-
-const generateResponse = (intent: PaymentIntent) => {
-	if (
-		intent.status === "requires_action" &&
-		// @ts-ignore
-		intent.next_action.type === "use_stripe_sdk"
-	) {
-		// Tell the client to handle the action
-		return {
-			requires_action: true,
-			payment_intent_client_secret: intent.client_secret,
-		};
-	} else if (intent.status === "succeeded") {
-		// The payment didn’t need any additional actions and completed!
-		// Handle post-payment fulfillment
-		return {
-			success: true,
-		};
-	} else {
-		// Invalid status
-		return {
-			error: "Invalid PaymentIntent status",
-		};
-	}
-};
